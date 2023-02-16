ebe0cddf922c8bd85b44e6a9b52cc03d65480d1d
Amós Silas <silastj@hotmail.com>
2023-02-15 23:04:16 -0300

    add first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// ESSE COMENTARIO ABAIXO CONVERTE O ARQUIVO EM .TS
+//@ts-check     
+const body = document.body
+
+body.style.background = '#000'
+
+const button = document.querySelector('button')
+is(button) button.click()
